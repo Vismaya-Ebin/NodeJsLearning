15516d3f4b184da776c7b1fa83014db61d0d8c75
Vismaya Wilson <vismayawilson3@gmail.com>
2022-04-11 12:31:27 +0400

    CORS dfix

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ async function createConnection() {
 }
 export const client = await createConnection();
 app.use(express.json());
-// app.use(cors());
+ app.use(cors());
 
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//   origin:'http://localhost:3000', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+//app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello Express 💕💕💕💕💕💕💕" + PORT);
